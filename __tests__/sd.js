8604b4287e07d5a70617ebe4858d0c9db119f877
mukul0000 <mukulrajput9675@gmail.com>
2021-12-13 18:59:55 +0530

    new

--- __tests__/sd.js
+++ __tests__/sd.js
@@ -6,6 +6,7 @@ const { sum } = require("../sum.js");
 const { miliSec } = require("../miliToSec.js");
 const { tax } = require("../tax.js");
 const { password } = require("../password.js");
+const { formatBytes } = require("../convert.js");
 
 test("addition of two positiove number", () => {
 	expect(sum(1, 2)).toBe(3);
@@ -17,11 +18,24 @@ test("milli secound to sec or min", () => {
 	expect(miliSec(180000)).toBe("3 min");
 });
 
+test("tax and savings", () => {
+	expect(tax(2000000)).toBe("tex:-600000 saving:-200000");
+	expect(tax(500000)).toBe("tex:-100000 saving:-150000");
+	expect(tax(800000)).toBe("tex:-160000 saving:-240000");
+});
+
 test("password validation check", () => {
 	expect(password("Mukul12@")).toBe("login scuessfull");
 	expect(password("12345")).toBe("your password is too short");
 	expect(password("muklhdgvh")).toBe("please check your password is weak");
 	expect(password("muklhdgvh@")).toBe("please check your password is weak");
+});
+
+test("convert data formatBytes", () => {
+	expect(formatBytes(1024)).toBe("1 KB");
+	expect(formatBytes("1024")).toBe("1 KB");
+	expect(formatBytes(1234)).toBe("1.205 KB");
+	expect(formatBytes(6544528)).toBe("6.241 MB");
 });
 
 // file extentions : js, ts,jsx,tsx
